66b546776effb0a365bcebf45619a9637391dc69
sruthy-kanth <sruthykanth281@gmail.com>
2026-06-04 14:27:32 +0530

    Added the Hamburger menu

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", function(){
+
+    navLinks.classList.toggle("active");
+
+});
